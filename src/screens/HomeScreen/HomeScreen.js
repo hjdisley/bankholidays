9db06f01a9352ffd609898e70f780865dc801798
Hayden Disley <hayden@pc-13-172.customer.ask4.lan>
2022-07-15 11:41:38 +0100

    Homescreen complete with tests

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,24 +1,68 @@
-import { View, Text, Button } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { Text } from "react-native";
 import Header from "../../components/Header/Header";
-import CountryDropdown from "../../components/CountryDropdown/CountryDropdown";
+import DivisionDropdown from "../../components/DivisionDropdown/DivisionDropdown";
 import { SafeAreaView } from "react-native-safe-area-context";
 import useFetchData from "../../hooks/useFetchData";
+import { HStack, Spinner, Heading, Center } from "native-base";
+import BankHolidayCard from "../../components/BankHolidayCard/BankHolidayCard";
 
 export default function HomeScreen({ navigation }) {
+  const [selectedDivision, setSelectedDivision] = useState("England And Wales");
+
   const { data, isLoading, error } = useFetchData(
     "https://www.gov.uk/bank-holidays.json"
   );
 
+  if (isLoading) {
+    return (
+      <HStack space={2} justifyContent="center">
+        <Spinner accessibilityLabel="Loading Bank Holidays" />
+        <Heading color="#583DFF" fontSize="md">
+          Loading...
+        </Heading>
+      </HStack>
+    );
+  }
+
+  if (error) {
+    <HStack space={2} justifyContent="center">
+      <Heading color="red" fontSize="md">
+        Error
+      </Heading>
+    </HStack>;
+  }
+
   return (
     <SafeAreaView>
       <Header />
-      <CountryDropdown />
-      <Button
-        testID="button"
-        title="Edit"
-        onPress={() => navigation.navigate("Edit")}
-      ></Button>
+      <DivisionDropdown
+        testID="DivisionDropdown"
+        divisions={Object.keys(data)}
+        selectedDivision={selectedDivision}
+        setSelectedDivision={setSelectedDivision}
+      />
+      <Center>
+        <Text style={{ fontSize: 25, fontWeight: "bold", marginBottom: 20 }}>
+          Upcoming Holidays
+        </Text>
+      </Center>
+
+      {data &&
+        !isLoading &&
+        data[selectedDivision]?.map((holiday, i) => {
+          return (
+            <Center>
+              <BankHolidayCard
+                testID={editButton}
+                data={data[selectedDivision]}
+                navigation={navigation}
+                key={i + holiday.title + holiday.date}
+                holiday={holiday}
+              />
+            </Center>
+          );
+        })}
     </SafeAreaView>
   );
 }
